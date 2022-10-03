ef6335d1f1fc08b1a28508b135ea17b92f4e1eb2
Elson <elsonnezaj2005@gmail.com>
2022-10-03 16:43:36 +0200

    updating our app with a shop page, collection preview component , collection item preview

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import { Route, Link, Switch } from 'react-router-dom'
+
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shoppage/shop.component.jsx'
+
 import './App.css'
 // const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
-
-const HatsPage = () => {
-  return (
-    <div>
-      <Link to="/">homepage</Link>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   )
