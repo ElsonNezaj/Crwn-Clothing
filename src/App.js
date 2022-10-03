3a9847f2c4f96630ef4e0ab907291cce9db6f006
Elson <elsonnezaj2005@gmail.com>
2022-10-03 16:58:15 +0200

    created header mavigation with links to home , shop pag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Route, Link, Switch } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shoppage/shop.component.jsx'
+import ShopPage from './pages/shoppage/shop.component'
+import Header from './components/header/header.component'
 
 import './App.css'
 // const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
@@ -10,6 +11,7 @@ import './App.css'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
