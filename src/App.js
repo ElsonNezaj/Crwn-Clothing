ce3229e5bc4e2ff83a4902aada9eaa7a8ef56413
Elson <elsonnezaj2005@gmail.com>
2022-10-03 17:16:08 +0200

    futja

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shoppage/shop.component'
 import Header from './components/header/header.component'
 
 import './App.css'
-// const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
 
 function App() {
   return (
