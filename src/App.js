9dd302cdf2febff3f5a756f60e70a01f915a6737
Elson <elsonnezaj2005@gmail.com>
2022-10-07 16:18:06 +0200

    c

--- src/App.js
+++ src/App.js
@@ -49,13 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route
-            exact
-            path="/signin"
-            // render={() =>
-            //   // currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            // }
-          />
+          <Route exact path="/signin" component={Account} />
         </Switch>
       </div>
     )
