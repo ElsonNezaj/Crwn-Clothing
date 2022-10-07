a0df970fb91d865d8e356631e998b641193f9775
Elson <elsonnezaj2005@gmail.com>
2022-10-07 16:12:03 +0200

    Changed app js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,80 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Switch, Redirect } from 'react-router'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shoppage/shop.component'
 import Header from './components/header/header.component'
-import Account from './pages/sign-in-and-sign-out/account.component'
-import {
-  auth,
-  signInWithGoogle,
-  createUserProfileDocument,
-} from './firebase/firebase.utils'
-import { onAuthStateChanged } from 'firebase/auth'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
+//selectors
+import { selectCurrentUser } from './redux/user/user.selectors'
+
+import { checkUserSession } from './redux/user/user.actions'
 
 import './App.css'
 
-class App extends React.Component {
-  constructor() {
-    super()
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
+    /**
+     * triggering the action checkUserSession
+     * the saga is listening to this action
+     */
+    checkUserSession()
+  }, [checkUserSession])
 
-    this.state = {
-      currentUser: null,
-    }
-  }
+  return (
+    <div>
+      <Header />
 
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user)
-      this.setState({ currentUser: user })
-
-      // createUserProfileDocument(user.displayName, user.email, user.uid)
-      console.log(user)
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth()
-  }
-
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={Account} />
-        </Switch>
-      </div>
-    )
-  }
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  )
 }
 
-export default App
+//allows access to the state
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+})
+
+// specifies actions your component might need to dispatch as props
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+/**------OBSERVABLE BASED---------
+     * onAuthStateChanged  firebase method that creates a stream
+     * whenever the authentication changes (a subscription is created)
+     * 
+     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        //console.log("PROPS", this.props);
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          //using redux for passing props
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+        });
+      }
+      setCurrentUser(userAuth);
+    });
+     */
